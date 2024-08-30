9d24cc73fe79d1b4ab919694179714c47ad24798
ManoMJ <max4761max@gmail.com>
2024-08-31 00:21:23 +0900

    ts added

--- learn-nextjs14/components/movie.tsx
+++ learn-nextjs14/components/movie.tsx
@@ -19,6 +19,6 @@ export default function Movie(
     }
    return ( <div className={styles.movie}>
                   <img src={poster_path} alt={title} onClick={onClick} />
-                  <Link href={`/movies/${id}`}>{title}</Link>
+                  <Link prefetch href={`/movies/${id}`}>{title}</Link>
                 </div>);
 }
